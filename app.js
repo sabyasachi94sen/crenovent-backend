3b804fb6ab4246cb0c0f8c8aa0706e16636feeac
sabyasachi94sen <sabyasachi9600@gmail.com>
2025-01-27 15:32:36 +0530

    7th commit

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const fs = require('fs');
 const { isJsonString } = require('./helper');
 const upload = multer({ dest: 'uploads/' }); // Temporary storage location
 const app = express();
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 
 
